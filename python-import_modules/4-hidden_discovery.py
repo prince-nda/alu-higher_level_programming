bcfb5f82c26469dc2291bafd966aaf16010069ba
p.ndahiro1@alustudent.com <p.ndahiro1@alustudent.com>
2025-06-24 11:11:02 -0700

    1-calculation.py

--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    import hidden_4 as extra
-
-
-for x in dir(extra):
-    if x[0:2] != "__":
-        print("{:s}".format(x))
--- python-import_modules/5-variable_load.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    import variable_load_5 as extra
-
-
-print("{:d}".format(extra.a))
