dfdda29ca06fe94455f48e739400100a5a7880ce
prince-nda <p.ndahiro1@alustudent.com>
2025-07-29 18:38:05 +0000

    changed codes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+"""Fetches https://0.0.0.0:5050/status using urllib"""
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://0.0.0.0:5050/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
