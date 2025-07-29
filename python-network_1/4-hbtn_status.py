cdfadf8d7de283630a65f02ca0652e3f9bc87283
prince-nda <p.ndahiro1@alustudent.com>
2025-07-29 19:33:00 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Python script that fetchs."""
-
-
+"""fetches https://intranet.hbtn.io/status."""
 import requests
+
+
 if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
