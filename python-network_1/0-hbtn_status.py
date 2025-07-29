586baa2c0318faefab5550b8b9cbfe1d70eff277
prince-nda <p.ndahiro1@alustudent.com>
2025-07-29 19:27:37 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
 """
+Module that fetches https://alu-intranet.hbtn.io/status
+
+This module uses urllib to make HTTP requests and displays information
+about the response body including its type, raw content, and UTF-8 decoded content.
+"""
+import urllib.request
 
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
+def fetch_status():
+    """
+    Fetches the status from https://alu-intranet.hbtn.io/status
+    and displays information about the response body.
+    """
+    url = 'https://alu-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    fetch_status()
