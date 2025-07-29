46e1c600f6500d63230d38bd500bb0da1fea0c8a
prince-nda <p.ndahiro1@alustudent.com>
2025-07-29 19:36:40 +0000

    update my file

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+Module that fetches status from a URL using requests package.
+
+This module uses the requests library to make HTTP requests and displays
+information about the response body including its type and content.
+The response text is automatically decoded by requests into a string.
+"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
