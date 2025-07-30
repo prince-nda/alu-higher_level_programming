adc7005d1f88311eae9cf9dc345647314654bce3
p.ndahiro1@alustudent.com <p.ndahiro1@alustudent.com>
2025-07-30 04:44:29 -0700

    added quotes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """A script that:
-- Fetches https://intranet.hbtn.io/status.
+- Fetches https://intranet.hbtn.io/status
 - Uses the urllib package.
 """
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
