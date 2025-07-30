30877798f0e24069b9defc1423096f87e1288445
p.ndahiro1@alustudent.com <p.ndahiro1@alustudent.com>
2025-07-30 11:36:58 -0700

    finish

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""Fetches https://intranet.hbtn.io/status using requests package."""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+    print("\t- utf8 content: {}".format(response.text))
