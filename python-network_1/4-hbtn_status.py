8651955f72ff1370c03952c8c3ef10a4251696bd
p.ndahiro1@alustudent.com <p.ndahiro1@alustudent.com>
2025-07-30 11:26:47 -0700

    done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,22 +8,7 @@ import requests
 
 
 if __name__ == "__main__":
-    urls = [
-        "https://intranet.hbtn.io/status",
-        "http://0.0.0.0:5050/status"
-    ]
-
-    for url in urls:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # Check for HTTP errors
-            
-            print("Body response:")
-            print("\t- type: {}".format(type(response.text)))
-            print("\t- content: {}".format(response.text))
-            print("\t- utf8 content: {}".format(response.text))
-            break
-        except requests.exceptions.RequestException:
-            continue
-    else:
-        print("Error: Could not fetch status from any URL.")
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
