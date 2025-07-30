73cbdedfaf1b24da6bdaef4efd71fcd023dc772b
p.ndahiro1@alustudent.com <p.ndahiro1@alustudent.com>
2025-07-30 11:13:22 -0700

    updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""A script that:
-- Fetches https://intranet.hbtn.io/status
-- Uses urllib package
-- Displays the response body in a specific format
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
 """
+
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    try:
-        with urllib.request.urlopen(req) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print("Error fetching status:", e)
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
