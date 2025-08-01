ba76e5bd2bfa5c26eef1a0e51a09707b49d9965a
p.ndahiro1@alustudent.com <p.ndahiro1@alustudent.com>
2025-08-01 04:58:56 -0700

    model state delete

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
 """
-Deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa.
-Usage: ./13-model_state_delete_a.py <mysql username> /
-                                    <mysql password> /
-                                    <database name>
+This script deletes all State objects with a name containing the letter 'a'
+from the database hbtn_0e_6_usa using SQLAlchemy ORM.
 """
+
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+def delete_states_with_a():
+    """
+    Connects to MySQL database using SQLAlchemy and deletes all State objects
+    that contain the letter 'a' in their name.
+    """
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database), pool_pre_ping=True)
+
+    # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
-        if "a" in state.name:
-            session.delete(state)
+    # Delete all State objects that contain the letter 'a'
+    session.query(State).filter(State.name.contains('a')).delete()
+
+    # Commit the changes
     session.commit()
+
+    # Close session
+    session.close()
+
+
+if __name__ == "__main__":
+    delete_states_with_a()
